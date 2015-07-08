e484482f0c53735093b7068952cf7a940bbe11a1
alex-simi-giacomelli <alex.simi@escolar.ifrn.edu.br>
2015-07-08 19:25:33 -0300

    ClienteUDP.rb
    Arquivo com os codigos do cliente

---
+++ ClienteUDP.rb
@@ -0,0 +1,25 @@
+﻿# Cliente UDP
+
+require 'socket'
+require 'timeout'
+
+$port = 12345
+$host = 'localhost'
+
+#inicializa o socket
+client = UDPSocket.new
+
+#conecta a um endereço
+client.connect($host, $port)
+
+#envia os dados p/ servidor
+puts "Qual eh a mensagem?"
+msg = gets.chomp
+client.send(msg, 0)
+
+#recebe os dados do servidor
+timeout(10) do
+	msg_rec = client.recvfrom(80)
+	puts "Nome: #{msg_rec[0]}\nProtocolo   Porta     End1        End2 \n#{msg_rec[1]}"
+	puts "A conexao foi encerrada."	
+end
